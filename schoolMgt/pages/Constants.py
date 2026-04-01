1b12a9f5e7c8d5b7e379091dc9af16861c6ea731
balakrishnan-ab <balakrishnan.ab@gmail.com>
2026-04-01 15:16:49 +0530

    Create Constants.py

---
+++ schoolMgt/pages/Constants.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+# மெயின் ஃபோல்டரைத் தேடும் பாதையில் (Path) சேர்த்தல்
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from Constants import BASE_URL
