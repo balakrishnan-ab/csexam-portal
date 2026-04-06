a6c3f4ca2b00875e7874a6f7009ab36bf2ee1be2
balakrishnan-ab <balakrishnan.ab@gmail.com>
2026-04-06 20:19:14 +0530

    Update utils.py

--- schoolMgt/utils.py
+++ schoolMgt/utils.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 def add_school_header():
     # இங்கே உங்கள் பள்ளியின் பெயரை மாற்றிக்கொள்ளுங்கள்
-    school_name = "அரசு மேல்நிலைப் பள்ளி, சென்னை" 
-    address = "123, கல்விச் சாலை, சென்னை - 600001"
+    school_name = "அரசு மேல்நிலைப் பள்ளி,தேவனாங்குறிச்சி " 
+    address = "திருச்செங்கோடு வட்டம், நாமக்கல் - 638209."
     
     st.markdown(f"""
         <div style="background-color:#1e3a8a; padding:20px; border-radius:10px; text-align:center; margin-bottom:20px;">
